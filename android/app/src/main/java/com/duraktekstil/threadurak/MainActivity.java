a1170881537b2d1262bf3550e2c4132ea5480683
goktemkirez <durak60@durak.com>
2021-07-27 13:35:04 +0300

    last v

--- android/app/src/main/java/com/duraktekstil/threadurak/MainActivity.java
+++ android/app/src/main/java/com/duraktekstil/threadurak/MainActivity.java
@@ -1,4 +1,4 @@
-package com.awesomeproject;
+package com.duraktekstil.threadurak;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "ThreaDurakApp";
+    return "ThreaDurak";
   }
 }
