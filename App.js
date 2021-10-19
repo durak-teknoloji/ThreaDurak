b362825e3868602cff1640e4d922af410fc5c2ab
goktemkirez <ogkirez@gmail.com>
2021-10-19 17:39:55 +0300

    added opening screen

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { NavigationContainer, DefaultTheme, DarkTheme} from '@react-navigation/native';
-import { useColorScheme } from 'react-native';
+import { useColorScheme, View, Image, ScrollView } from 'react-native';
 import Tabs from './navigation/tabs';
 
 const App = () => {
-  const scheme = useColorScheme();
+  // const scheme = useColorScheme();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getPlaylists();
+  }, []);
+
+  const getPlaylists = async () => {
+    try {
+      setLoading(true);
+    } catch (error) {
+      console.log("error", error);
+    } finally {
+      setTimeout(() => {
+        setLoading(false);
+      }, 2500);
+    }
+  };
+
   return(
-    <NavigationContainer>
-      <Tabs></Tabs>
-    </NavigationContainer>
+    <>
+      {
+        loading ? (
+          <View style={{ flex: 1 }}>
+            <Image source={require('./assets/Images/OpeningScreen.png')} resizeMode='cover'
+              style={{
+                flex: 1,
+                width: '100%',
+                backgroundColor: '#D7DFE6',
+              }}></Image></View>
+        ) : (
+          <NavigationContainer>
+            <Tabs></Tabs>
+          </NavigationContainer>
+        )
+      }
+    </>
     //  theme={scheme === 'dark' ? DarkTheme : DefaultTheme}
   );
 }
